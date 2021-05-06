beeac303ff478f6989ab324b7ab7e73e84abc7d5
Bhumkar <Pravin.Bhumkar@emc.com>
2021-05-06 17:26:25 +0530

    Added fake headers to avoid any IP blocking

--- api.py
+++ api.py
@@ -1,16 +1,23 @@
 import requests
 import json
 
+from fake_headers import Headers
+
 
 class APIClient(object):
     def __init__(self):
         self.base_url = "https://cdn-api.co-vin.in/api"
-        self.headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"}
+        self.headers = Headers(
+            browser="chrome",
+            os="win",
+            headers=True
+        )
 
     def session(self):
         self.s = requests.Session()
 
 
     def get(self, method, params=None):
-        response = self.s.get(self.base_url + method, params=params, verify=False, headers=self.headers)
+        response = self.s.get(self.base_url + method, params=params,
+            verify=False, headers=self.headers.generate())
         return (response.text, response.status_code)
